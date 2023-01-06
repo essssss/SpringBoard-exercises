5e4a9e89a1e8fa934af289da410a02d1a5e27722
essssss <srmcnamara7@gmail.com>
2023-01-06 14:04:59 -0500

    meme generator

---
+++ 8 Meme Generator/app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("#meme-generator");
+const imageURL = form.elements["url"];
+const topText = form.elements["top"];
+const bottomText = form.elements["bottom"];
+const results = document.querySelector("#results");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const newMeme = makeMeme(imageURL.value, topText.value, bottomText.value);
+  results.appendChild(newMeme);
+});
+
+function makeMeme(pic, toptext, bottomtext) {
+  const memeDiv = document.createElement("div");
+  memeDiv.setAttribute("id", "memeDiv");
+  const topTextContent = document.createElement("p");
+  topTextContent.innerText = toptext;
+  topTextContent.setAttribute("id", "topTextBox");
+  const meme = document.createElement("img");
+  meme.setAttribute("src", pic);
+  meme.setAttribute("id", "memeIMG");
+  const bottomTextContent = document.createElement("p");
+  bottomTextContent.innerText = bottomtext;
+  bottomTextContent.setAttribute("id", "bottomTextBox");
+  memeDiv.appendChild(topTextContent);
+  memeDiv.appendChild(meme);
+  memeDiv.appendChild(bottomTextContent);
+  return memeDiv;
+}
+
+results.addEventListener("click", function (e) {
+  e.target.parentElement.remove();
+});
+
+// friendList.addEventListener('click', function(e){
+//   if(e.target.tagName === 'BUTTON'){
+//       e.target.parentElement.remove();
+//   }
